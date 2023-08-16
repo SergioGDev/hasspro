63f9c1794a35cabf49437f03ee784630dbd14218
SergioGDev <admin@sergiogdev.com>
2023-08-16 16:06:00 +0200

    feat: despliegue 16 agosto

--- src/app/interfaces/local-data.interface.ts
+++ src/app/interfaces/local-data.interface.ts
@@ -12,6 +12,7 @@ export interface DatosServicios {
     iconPath: string;
     datos: string[];
     texto?: string;
+    pathImg?: string;
   }
   
 
--- src/app/pages/gestion-participativa-sala/gestion-participativa-sala.component.ts
+++ src/app/pages/gestion-participativa-sala/gestion-participativa-sala.component.ts
@@ -4,59 +4,106 @@ import { DatosServicios } from 'src/app/interfaces/local-data.interface';
 @Component({
   selector: 'app-gestion-participativa-sala',
   templateUrl: './gestion-participativa-sala.component.html',
-  styleUrls: ['./gestion-participativa-sala.component.scss']
+  styleUrls: ['./gestion-participativa-sala.component.scss'],
 })
 export class GestionParticipativaSalaComponent implements OnInit {
+  vControlHorarios: string[] = ['Horarios', 'Horas', 'App'];
+  vControlProveedores: string[] = [
+    'Pedidos',
+    'Control de stock',
+    'Proveedores',
+    'App',
+  ];
+  vGestionCartas: string[] = [
+    'Cartas',
+    'Digitalizacion de cartas',
+    'Inventarios',
+    'Escandallos',
+    'App',
+  ];
+  vPersonal: string[] = [
+    'Charlas',
+    'Formación participativa',
+    'Organización y gestión del equipo',
+  ];
+  vServicio: string[] = [
+    'Alérgenos',
+    'Gestión antes y después del servicio',
+    'Gestión de reservas',
+    'Organización de mesas',
+    'Tiempos',
+    'Tipos de comanda',
+    'Tipos de servicios',
+    'App',
+  ];
+  vGestionEventos: string[] = [
+    'Desarrollo del evento',
+    'Coordinador de eventos',
+    'Menús',
+    'App',
+  ];
+  vImagenes: string[] = [
+    'Asesoramiento (auditoría)',
+    'Gestión de redes sociales',
+    'Creación de la web',
+    'Diseño de la imagen de la marca',
+  ];
 
-
-  vControlHorarios: string[] = [ 'Horarios', 'Horas', 'App' ]
-  vControlProveedores: string[] =[ 'Pedidos', 'Control de stock', 'Proveedores', 'App' ]
-  vGestionCartas: string[] = [ 'Cartas', 'Digitalizacion de cartas', 'Inventarios', 'Escandallos', 'App' ];
-  vPersonal: string[] = [ 'Charlas', 'Formación participativa', 'Organización y gestión del equipo' ];
-  vServicio: string[] = [ 'Alérgenos', 'Gestión antes y después del servicio', 'Gestión de reservas', 
-    'Organización de mesas', 'Tiempos', 'Tipos de comanda', 'Tipos de servicios', 'App' ];
-  vGestionEventos: string[] = [ 'Desarrollo del evento', 'Coordinador de eventos', 'Menús', 'App' ];
-  
   vDatos: DatosServicios[] = [
     {
       titulo: 'Gestión y control de horarios y horas',
       iconPath: '../../../assets/icons/icon-clock.png',
-      datos: this.vControlHorarios
+      pathImg:
+        '../../../assets/gestion-participativa-sala/horarios_y_horas.webp',
+      datos: this.vControlHorarios,
     },
     {
       titulo: 'Gestión y control de proveedores y stock',
       iconPath: '../../../assets/icons/icon-stock.png',
-      datos: this.vControlProveedores
+      pathImg: '../../../assets/gestion-participativa-sala/proveedores.webp',
+      datos: this.vControlProveedores,
     },
     {
       titulo: 'Gestión y organización de cartas',
       iconPath: '../../../assets/icons/icon-message.png',
-      datos: this.vGestionCartas
+      pathImg: '../../../assets/gestion-participativa-sala/gestion_cartas.webp',
+      datos: this.vGestionCartas,
     },
     {
       titulo: 'Selección y formación del personal',
       iconPath: '../../../assets/icons/icon-person.png',
-      datos: this.vPersonal
+      pathImg:
+        '../../../assets/gestion-participativa-sala/seleccion_formacion_personal.webp',
+      datos: this.vPersonal,
     },
     {
       titulo: 'Gestión y organizacion del servicio',
       iconPath: '../../../assets/icons/icon-service.png',
-      datos: this.vServicio
+      pathImg:
+        '../../../assets/gestion-participativa-sala/organizacion_servicio.webp',
+      datos: this.vServicio,
     },
     {
       titulo: 'Organización y gestión de eventos',
       iconPath: '../../../assets/icons/icon-events.png',
-      datos: this.vGestionEventos
+      pathImg:
+        '../../../assets/gestion-participativa-sala/gestion_eventos.webp',
+      datos: this.vGestionEventos,
     },
-  ]
+    {
+      titulo: 'Imagen',
+      iconPath: '../../../assets/icons/icon-events.png',
+      pathImg: '../../../assets/gestion-participativa-sala/imagen.webp',
+      datos: this.vImagenes,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    window.scroll({ 
-      top: 0, 
-      left: 0
+    window.scroll({
+      top: 0,
+      left: 0,
     });
   }
-
 }
